7554b2a621ca1c8d9b29832337a3b38bdaf5529a
Ahmet Bakan <lordnapi@gmail.com>
2014-04-27 16:55:58 -0700

    Initial commit.

---
+++ ablog/__init__.py
@@ -0,0 +1,378 @@
+# -*- coding: utf-8 -*-
+"""ABlog for Sphinx"""
+
+import os
+from datetime import datetime
+
+from docutils import nodes
+from docutils.parsers.rst import directives
+from sphinx.util.nodes import set_source_info
+from sphinx.util.compat import Directive
+from sphinx.environment import dummy_reporter
+
+from .ablog import ABlog, CONFIG
+
+
+class PostNode(nodes.Element):
+    """Represent ``post`` directive content and options in document tree."""
+
+    pass
+
+
+class PostList(nodes.General, nodes.Element):
+    """Represent ``postlist`` directive converted to a list of links."""
+
+    pass
+
+
+class PostDirective(Directive):
+    """Handle ``post`` directives."""
+
+    has_content = True
+    required_arguments = 0
+    optional_arguments = 1
+    final_argument_whitespace = True
+    option_spec = {
+        'tags': lambda a: [s.strip() for s in a.split(',')],
+        'author': lambda a: [s.strip() for s in a.split(',')],
+        'category': lambda a: [s.strip() for s in a.split(',')],
+        'location': lambda a: [s.strip() for s in a.split(',')],
+        'redirect': lambda a: [s.strip() for s in a.split(',')],
+        'title': lambda a: a.strip(),
+        'update': lambda a: a.strip(),
+        'image': int,
+        'excerpt': int,
+        'exclude': directives.flag,
+    }
+
+    def run(self):
+
+        node = PostNode()
+        node.document = self.state.document
+        set_source_info(self, node)
+        self.state.nested_parse(self.content, self.content_offset,
+                                node, match_titles=1)
+
+        node['date'] = self.arguments[0] if self.arguments else None
+        node['update'] = self.options.get('update', None)
+        node['tags'] = self.options.get('tags', [])
+        node['author'] = self.options.get('author', [])
+        node['category'] = self.options.get('category', [])
+        node['location'] = self.options.get('location', [])
+        node['redirect'] = self.options.get('redirect', [])
+        node['title'] = self.options.get('title', None)
+        node['image'] = self.options.get('image', None)
+        node['excerpt'] = self.options.get('excerpt', None)
+        node['exclude'] = 'exclude' in self.options
+        return [node]
+
+
+class PostListDirective(Directive):
+    """Handle ``postlist`` directives."""
+
+    has_content = False
+    required_arguments = 0
+    optional_arguments = 1
+    final_argument_whitespace = False
+    option_spec = {
+        'tags': lambda a: set(s.strip() for s in a.split(',')),
+        'author': lambda a: set(s.strip() for s in a.split(',')),
+        'category': lambda a: set(s.strip() for s in a.split(',')),
+        'location': lambda a: set(s.strip() for s in a.split(',')),
+        'reverse': directives.flag,
+    }
+
+    def run(self):
+
+        node = PostList()
+        node.document = self.state.document
+        set_source_info(self, node)
+        self.state.nested_parse(self.content, self.content_offset,
+                                node, match_titles=1)
+
+        node['length'] = int(self.arguments[0]) if self.arguments else None
+        node['tags'] = self.options.get('tags', [])
+        node['author'] = self.options.get('author', [])
+        node['category'] = self.options.get('category', [])
+        node['location'] = self.options.get('location', [])
+        node['reverse'] = 'reverse' in self.options
+        return [node]
+
+
+def purge_posts(app, env, docname):
+    """Remove post and reference to it in the standard domain when its
+    document is removed or changed."""
+
+    if hasattr(env, 'ablog_posts'):
+        env.ablog_posts.pop(docname, None)
+    env.domains['std'].data['labels'].pop(os.path.split(docname)[1], None)
+
+
+def process_posts(app, doctree):
+    """Process posts and map posted document names to post details in the
+    environment."""
+
+    env = app.builder.env
+    if not hasattr(env, 'ablog_posts'):
+        env.ablog_posts = {}
+    ablog = app.ablog
+    post_nodes = list(doctree.traverse(PostNode))
+    if not post_nodes:
+        return
+    docname = env.docname
+    node = post_nodes[0]
+    if len(post_nodes) > 1:
+        env.warn(docname, 'multiple post directives found, '
+                 'first one is considered')
+
+    # Making sure that post has a title because all post titles
+    # are needed when resolving post lists in documents
+    title = node['title']
+    if not title:
+        for title in doctree.traverse(nodes.title):
+            break
+        # A problem with the following is that title may contain pending
+        # references, e.g. :ref:`tag-tips`
+        title = title.astext()
+
+    # creating a summary here, before references are resolved
+    excerpt = []
+    if node.children:
+        if node['exclude']:
+            node.replace_self([])
+        else:
+            node.replace_self(node.children)
+        for child in node.children:
+            excerpt.append(child.deepcopy())
+    else:
+        count = 0
+        for nod in doctree.traverse(nodes.paragraph):
+            excerpt.append(nod.deepcopy())
+            count += 1
+            if count >= node['excerpt']:
+                break
+        node.replace_self([])
+    if node['image']:
+        count = 0
+        for nod in doctree.traverse(nodes.image):
+            count += 1
+            if count == node['image']:
+                excerpt.append(nod.deepcopy())
+                break
+
+    date = node['date']
+    if date:
+        try:
+            date = datetime.strptime(date, ablog.post_date_format)
+        except ValueError:
+            raise ValueError('invalid post published date in: ' + docname)
+    else:
+        date = None
+
+    update = node['update']
+    if update:
+        try:
+            update = datetime.strptime(update, ablog.post_date_format)
+        except ValueError:
+            raise ValueError('invalid post update date in: ' + docname)
+    else:
+        update = date
+
+    env.ablog_posts[docname] = postinfo = {
+        'date': date,
+        'update': update,
+        'title': title,
+        'excerpt': excerpt,
+        'tags': node['tags'],
+        'author': node['author'],
+        'category': node['category'],
+        'location': node['location'],
+        'redirect': node['redirect'],
+        'image': node['image'],
+        'exclude': node['exclude']}
+
+    # if docname ends with `index` use folder name to reference the document
+    # a potential problem here is that there may be files/folders with the
+    #   same name, so issuing a warning when that's the case may be a good idea
+    folder, label = os.path.split(docname)
+    if label == 'index':
+        folder, label = os.path.split(folder)
+    app.env.domains['std'].data['labels'][label] = (docname, label, title)
+
+    # mark the post as 'orphan' so that
+    #   "document isn't included in any toctree" warning is not issued
+    app.env.metadata[docname]['orphan'] = True
+
+    # instantiate catalogs and collections here
+    #  so that references are created and no warnings are issued
+
+    for key in ['tags', 'author', 'category', 'location']:
+        catalog = app.ablog.catalogs[key]
+        for label in postinfo[key]:
+            catalog[label]
+    if postinfo['date']:
+        app.ablog.archive[postinfo['date'].year]
+
+
+
+def process_postlist(app, doctree, docname):
+    """Replace `PostList` nodes with lists of posts. Also, register all posts
+    if they have not been registered yet."""
+
+    if not app.ablog:
+        register_posts(app)
+
+    for node in doctree.traverse(PostList):
+        posts = list(app.ablog.recent(node.attributes['length'], docname,
+                                      **node.attributes))
+        if node.attributes['reverse']:
+            posts.sort() # in reverse chronological order, so no reverse=True
+        bl = nodes.bullet_list()
+        for post in posts:
+            bli = nodes.list_item()
+            bl.append(bli)
+            par = nodes.paragraph()
+
+            if True:
+                par.append(nodes.Text(
+                    post.date.strftime(app.ablog.post_date_format) + ' - '))
+
+            bli.append(par)
+            ref = nodes.reference()
+            ref['refuri'] = post.docname
+            ref['ids'] = []
+            ref['backrefs'] = []
+            ref['dupnames'] = []
+            ref['classes'] = []
+            ref['names'] = []
+            ref['internal'] = True
+            par.append(ref)
+
+            emp = nodes.emphasis()
+            ref.append(emp)
+            emp.append(nodes.Text(post.title))
+
+        node.replace_self(bl)
+
+
+def generate_archive_pages(app):
+    """Generate archive pages for all posts, categories, tags, authors, and
+    drafts."""
+
+    ablog = app.ablog
+    for post in ablog.posts:
+        for redirect in post.redirect:
+            yield (redirect, {'redirect': post.docname, 'post': post},
+                   'redirect.html')
+
+    for title, catalog in [
+        ('Posts by', ablog.author),
+        ('Posts from', ablog.location),
+        ('Posts in', ablog.category),
+        ('Posted in', ablog.archive),
+        ('Posts tagged', ablog.tags),]:
+
+        if not len(catalog):
+            continue
+
+        context = {
+            'parents': [],
+            'title': title,
+            'catalog': catalog,
+            'summary': True,
+        }
+        yield (catalog.docname, context, 'catalog.html')
+
+        for collection in catalog:
+
+            if not len(collection):
+                continue
+
+            context = {
+                'parents': [],
+                'title': title,
+                'archive': collection,
+                'summary': True,
+            }
+            yield (collection.docname, context, 'archive.html')
+
+    for title, collection in [('Posts', ablog.posts),
+                             ('Drafts', ablog.drafts)]:
+        context = {
+            'parents': [],
+            'title': '',
+            'archive': collection,
+            'summary': True,
+        }
+        yield (collection.docname, context, 'archive.html')
+
+    d = ablog.std_domain.data#['labels']
+    #l = d.keys()
+    #l.sort()
+    #for k in l:
+    #    print k, d[k]if
+    url = ablog.blog_baseurl
+    if not url:
+        return
+
+    from werkzeug.contrib.atom import AtomFeed
+    feed_path  = os.path.join(app.builder.outdir, ablog.blog_path, 'atom.xml')
+    feed = AtomFeed(ablog.blog_title,
+                    title_type='text',
+                    url=url,
+                    feed_url=os.path.join(url, feed_path),
+                    subtitle=ablog.blog_feed_subtitle)
+    from datetime import datetime
+    for post in ablog.posts:
+        feed.add(post.title,
+                 content=post.summary(ablog.blog_path),
+                 title_type='text',
+                 content_type='html',
+                 author=', '.join(a.name for a in post.author),
+                 url=os.path.join(url, post.docname),
+                 id='post.uid' + str(post),
+                 updated=post.update, published=post.date)
+
+    with open(feed_path, 'w') as out:
+        out.write(feed.to_string())
+
+
+
+def init_ablog(app):
+    """Instantiate ABlog, link from Sphinx app and `html_context`, and
+    register posts."""
+
+    # include in html context so that it can be reached from templates
+    app.config.html_context['ablog'] = app.ablog = ABlog(app)
+
+
+def register_posts(app):
+
+    from sphinx.util.console import bold, purple, darkgreen, term_width_line
+    ablog_posts = getattr(app.env, 'ablog_posts', {})
+    iterator = ablog_posts.keys()
+    for docname in app.builder.status_iterator(iterator,
+        'registering posts... ', purple, 50):
+        app.ablog.register(docname, ablog_posts[docname])
+
+
+def setup(app):
+    """Setup ABlog extension."""
+
+    for args in CONFIG:
+        app.add_config_value(*args)
+
+    app.add_directive('post', PostDirective)
+    app.add_directive('postlist', PostListDirective)
+    app.connect('builder-inited', init_ablog)
+    app.connect('doctree-read', process_posts)
+    app.connect('env-purge-doc', purge_posts)
+    app.connect('doctree-resolved', process_postlist)
+    app.connect('html-collect-pages', generate_archive_pages)
+
+
+def get_html_templates_path():
+    """Return path to the folder containing ABlog templates."""
+
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                        'templates')
---
+++ ablog/ablog.py
@@ -0,0 +1,464 @@
+"""ABlog classes for handling posts and archives."""
+
+import os
+import re
+
+from datetime import datetime
+from unicodedata import normalize
+
+from docutils import nodes
+from docutils.io import StringOutput
+
+from sphinx.util.osutil import relative_uri
+from sphinx.environment import dummy_reporter
+
+def slugify(s):
+    """Slugify *s*."""
+
+    n = normalize('NFKD', unicode(s)).encode('ascii', 'ignore')
+    return re.sub(r'[-\s]+', '-', re.sub(r'[^\w\s-]', '', n).strip().lower())
+
+DEBUG = True
+CONFIG = [
+    ('blog_path', 'blog', True),
+    ('blog_title', 'Blog', True),
+    ('blog_baseurl', None, True),
+
+    ('blog_feed_subtitle', None, True),
+    ('blog_authors', {}, True),
+    ('blog_default_author', None, True),
+    ('blog_locations', {}, True),
+    ('blog_default_location', None, True),
+
+    ('fontawesome_link_cdn', False, True),
+    ('fontawesome_included', False, True),
+    ('fontawesome_css_file', False, True),
+
+    ('post_date_format', '%b %d, %Y', True),
+    ('post_auto_image', 0, True),
+    ('post_auto_excerpt', 1, True),
+    ('post_redirect_refresh', 5, True),
+
+    ('disqus_shortname', None, True),
+    ('disqus_drafts', False, True),
+    ('disqus_pages', False, True),
+]
+
+
+_ = datetime.today()
+TOMORROW = datetime(_.year, _.month, _.day + 1)
+FUTURE = datetime(9999, 12, 31)
+
+
+class ABlog(object):
+
+    """Handle blog operations."""
+
+    def __init__(self, app):
+
+        self.app = app
+        self.std_domain = self.app.env.domains['std']
+        self.config = {}
+
+        for opt in CONFIG:
+            self.config[opt[0]] = getattr(app.config, opt[0])
+
+
+
+        opt = self.config['blog_default_author']
+        if opt is not None and not isinstance(opt, list):
+            self.config['blog_default_author'] = [opt]
+
+        opt = self.config['blog_default_location']
+        if opt is not None and not isinstance(opt, list):
+            self.config['blog_default_location'] = [opt]
+
+        self.blog = Catalog(self, 'blog', 'blog', None)
+        self.archive = Catalog(self, 'archive', 'archive', None)
+
+        self.catalogs = cat = {}  # catalogs of users set labels
+        self.tags = cat['tags'] = Catalog(self, 'tags', 'tag', 'tag')
+        self.author = cat['author'] = Catalog(self, 'author',
+            'author', 'author')
+        self.location = cat['location'] = Catalog(self, 'location',
+            'location',  'location')
+        self.category = cat['category'] = Catalog(self, 'category',
+            'category', 'category')
+
+        for catname in ['author', 'location']:
+            catalog = self.catalogs[catname]
+            items = self.config['blog_' + catname + 's'].items()
+            for label, (name, link) in items:
+                catalog[label] = Collection(catalog, label, name, link)
+
+
+        self.posts = self.blog['post'] = Collection(self.blog, 'post',
+            'Posts', path=self.blog_path)
+        self.drafts = self.blog['draft'] = Collection(self.blog, 'draft',
+            'Drafts', path=os.path.join(self.blog_path, 'drafts'))
+        # add references to posts and drafts
+        # e.g. :ref:`blog-posts`
+        domain = self.app.env.domains['std']
+        domain.data['labels']['blog-posts'] = (
+            os.path.join(self.config['blog_path'], 'index'), '', 'Posts')
+        domain.data['labels']['blog-drafts'] = (
+            os.path.join(self.config['blog_path'], 'drafts', 'index'), '',
+                         'Drafts')
+        domain.data['labels']['blog-feed'] = (
+            os.path.join(self.config['blog_path'], 'atom.xml'), '',
+            self.blog_title + ' Feed')
+
+        self.config['cloud_size'] = 5
+        self.config['fontawesome'] = (self.config['fontawesome_included'] or
+                                      self.config['fontawesome_link_cdn'] or
+                                      self.config['fontawesome_css_file'])
+
+    def __getattr__(self, name):
+
+        try:
+            attr = self.config[name]
+        except KeyError:
+            raise AttributeError('ABlog has no configuration option {}'
+                                 .format(repr(name)))
+        return attr
+
+    def __getitem__(self, key):
+
+        return self.posts[key] or self.drafts[key]
+
+    def __contains__(self, item):
+
+        return item in self.posts or item in self.drafts
+
+    def __len__(self):
+
+        return len(self.posts)
+
+    def __nonzero__(self):
+
+        return len(self) > 0
+
+    @property
+    def feed_path(self):
+        """RSS feed page name."""
+
+        return os.path.join(self.blog_path, 'rss.xml')
+
+    def register(self, docname, info):
+        """Register post *docname*."""
+
+
+        post = Post(self, docname, info)
+        if post.date and post.date < TOMORROW:
+            self.posts.add(post)
+        else:
+            self.drafts.add(post)
+        for catalog in self.catalogs.values():
+            catalog.add(post)
+
+    def recent(self, num, docname=None, **labels):
+        """Yield *num* recent posts, excluding the one with `docname`."""
+
+        for i, post in enumerate(self.posts):
+            if post.docname == docname:
+                num += 1
+                continue
+            if i == num:
+                return
+            yield post
+
+    def link_posts(self):
+        """Link posts after sorting them post by published date."""
+
+        posts = list(self.posts)
+        posts.sort()
+        posts[0].prev = posts[-1].next = None
+        for i in range(1, len(posts)):
+            post = posts[i]
+            posts[i - 1].next = post
+            post.prev = posts[i - 1]
+
+
+def html_builder_write_doc(self, docname, doctree):
+    """Part of :meth:`sphinx.builders.html.StandaloneHTMLBuilder.write_doc`
+    method used to convert *doctree* to HTML."""
+
+    destination = StringOutput(encoding='utf-8')
+    doctree.settings = self.docsettings
+
+    self.secnumbers = {}
+    self.imgpath = relative_uri(self.get_target_uri(docname), '_images')
+    self.dlpath = relative_uri(self.get_target_uri(docname), '_downloads')
+    self.current_docname = docname
+    self.docwriter.write(doctree, destination)
+    self.docwriter.assemble_parts()
+    return self.docwriter.parts['fragment']
+
+
+class Post(object):
+
+    """Handle post metadata."""
+
+    def __init__(self, ablog, docname, info):
+
+
+        self.ablog = ablog
+        self.docname = docname
+        self.date = date = info['date']
+        self.update = info['update']
+        self.published = date and date < TOMORROW
+        self.draft = not self.published
+        self.title = info['title']
+        self.excerpt = info['excerpt']
+        self._next = self._prev = -1
+
+        #self.language = info.get('language')
+
+        # archives
+        self.blog = []
+        if self.published:
+            self.tags = info.get('tags')
+            self.author = info.get('author')
+            self.category = info.get('category')
+            self.location = info.get('location')
+
+            if not self.author and ablog.blog_default_author:
+                self.author = ablog.blog_default_author
+            if not self.location and ablog.blog_default_location:
+                self.location = ablog.blog_default_location
+
+            self.archive = self.ablog.archive[self.date.year]
+            self.archive.add(self)
+
+        else:
+            self.tags = info.get('tags')
+            self.author = info.get('author')
+            self.category = info.get('category')
+            self.location = info.get('location')
+            self.archive = []
+
+        self.redirect = info.get('redirect')
+
+        self.options = info
+
+    def __lt__(self, other):
+
+        return (self.date or FUTURE) < (other.date or FUTURE)
+
+
+    def titles(self, pagename):
+        """Return title after resolving references with respect to
+        *pagename*."""
+
+        app = self.ablog.app
+        builder = app.builder
+        title = self._title.deepcopy()
+        app.env.resolve_references(title, pagename, builder)
+        return html_builder_write_doc(builder, pagename, title)
+
+    def summary(self, pagename):
+        """Return summary after resolving references with respect to
+        *pagename*."""
+
+        doctree = nodes.document({}, dummy_reporter)
+        for node in self.excerpt:
+            doctree.append(node.deepcopy())
+        app = self.ablog.app
+        app.env.resolve_references(doctree, pagename, app.builder)
+        return html_builder_write_doc(app.builder, pagename, doctree)
+
+    def _summarize(self, doctree, npars, image):
+        """Create a summary of the post."""
+
+        p_count = 0
+        i = 0
+        title_visited = False
+        doctree = doctree.deepcopy()
+        remove = []
+        for node in doctree.traverse():
+            if isinstance(node, nodes.title) and not title_visited:
+                title_visited = True
+                remove.append(node)
+                continue
+            if isinstance(node, nodes.paragraph):
+                p_count += 1
+            if isinstance(node, (nodes.image, nodes.figure)):
+                i += 1
+                if i != image:
+                    remove.append(node)
+            elif p_count >= npars:
+                remove.append(node)
+        for node in remove:
+            node.parent.remove(node)
+
+        self._summary = doctree
+
+    @property
+    def next(self):
+        """Next published post in chronological order."""
+
+        if self._next == -1:
+            self.ablog.link_posts()
+        return self._next
+
+    @next.setter
+    def next(self, post):
+        """Set next published post in chronological order."""
+
+        self._next = post
+
+    @property
+    def prev(self):
+        """Previous published post in chronological order."""
+
+        if self._prev == -1:
+            self.ablog.link_posts()
+        return self._prev
+
+    @prev.setter
+    def prev(self, post):
+        """Set previous published post in chronological order."""
+
+        self._prev = post
+
+
+class Catalog(object):
+
+    """Handles collections of posts."""
+
+    def __init__(self, blog, name, xref, path):
+
+        self.blog = blog
+        self.name = name
+        self.xref = xref # for creating labels, e.g. `tag-python`
+        self.collections = {}
+
+        if path:
+            self.path = self.docname = os.path.join(blog.blog_path, path)
+        else:
+            self.path = self.docname = blog.blog_path
+
+        self._lens = None
+        self._minmax = None
+
+    def __getitem__(self, name):
+
+        try:
+            return self.collections[name]
+        except KeyError:
+            return self.collections.setdefault(name, Collection(self, name))
+
+    def __setitem__(self, name, item):
+
+        self.collections[name] = item
+
+    def __len__(self):
+
+        return sum(len(coll) for coll in self)
+
+    def __nonzero__(self):
+
+        return len(self) > 0
+
+    def __iter__(self):
+
+        keys = list(self.collections)
+        keys.sort()
+        for key in keys:
+            yield self.collections[key]
+
+    def add(self, post):
+        """Add post to appropriate collection(s) and replace collections
+        labels with collection objects."""
+
+        colls = []
+        for label in getattr(post, self.name, []):
+            coll = self[label]
+            if post.published:
+                coll.add(post)
+            colls.append(coll)
+        setattr(post, self.name, colls)
+
+    def minmax(self):
+        """Return minimum and maximum sizes of collections."""
+
+        if self._lens is None or len(self._lens) != len(self.collections):
+            self._lens = [len(coll) for coll in self.collections.values()
+                          if len(coll)]
+            self._minmax = min(self._lens), max(self._lens)
+        return self._minmax
+
+
+class Collection(object):
+
+    """Posts sharing a label, i.e. tag, category, author, or location."""
+
+    def __init__(self, catalog, label, name=None, href=None, path=None):
+
+        self.catalog = catalog
+        self.label = label
+        self.name = name or self.label
+        self.href = href
+
+        self._posts = {}
+
+        self._path = path
+        self.xref = self.catalog.xref + '-' + slugify(label)
+        self._slug = None
+        self._html = None
+
+        self.catalog.blog.std_domain.data['labels'][self.xref] = (
+            self.docname, self.xref, self.name)
+
+    def __str__(self):
+
+        return self.name
+
+    def __len__(self):
+
+        return len(self._posts)
+
+    def __nonzero__(self):
+
+        return len(self) > 0
+
+    def __unicode__(self):
+
+        return unicode(self.name)
+
+    def __iter__(self):
+
+        posts = list(self._posts.values())
+        posts.sort(reverse=True)
+        for post in posts:
+            yield post
+
+    def __getitem__(self, key):
+
+        return self._posts.get(key)
+
+    def __contains__(self, item):
+
+        return item in self._posts
+
+    def add(self, post):
+        """Add post to the collection."""
+
+        self._posts[post.docname] = post
+
+    @property
+    def relsize(self):
+        """Relative size used in tag clouds."""
+
+        min_, max_ = self.catalog.minmax()
+        size = int((len(self) - min_) / (max_ - min_) *
+                   (self.catalog.blog.cloud_size - 1)) + 1
+        return size
+
+    @property
+    def docname(self):
+        """Collection page document name."""
+
+        if self._path is None:
+            self._path = os.path.join(self.catalog.path, slugify(self.name))
+        return self._path
