6484713c337dae60664c9b7f60029d38de298c18
Mehmet Gerceker <mehmetg@msn.com>
2015-02-04 18:20:00 -0800

    Added ablog "post" command with 2 options title and body. Other options may be added later...

--- ablog/commands.py
+++ ablog/commands.py
@@ -132,6 +132,85 @@ subparser.add_argument('-p', '--port', dest='port', type=int,
 subparser.set_defaults(func=lambda ns: ablog_serve(**ns.__dict__))
 subparser.set_defaults(subparser=subparser)
 
+def ablog_post(subparser, **kwargs):
+
+    POST_TEMPLATE =u'''
+%(post_title)s
+====================
+.. post:: %(post_date)s
+'''
+
+    # We may need to augment this to use a specified folder
+    blog_root = find_confdir()
+    if(not blog_root):
+        print('Configuration file (conf.py) could not be found '
+            'in the current working directory and its parents.')
+        sys.exit(1)
+    
+
+   
+    from datetime import date
+    from os import path
+
+    #Generate basic post params.
+    today = date.today()
+    title = kwargs['post_title'].split('.')[0]
+    body = kwargs['post_body']
+    file_name = "{}_{}.rst".format(title.replace(' ', '_'), today).lower()
+    file_path = path.join(blog_root, file_name)
+    today = today.strftime("%b %d, %Y")
+    
+    pars = {'post_date': today,
+            'post_title': title
+            }
+    
+    print('Creating blog post in %s' % blog_root)
+
+    #Check if we have the file in the target folder and rename accordingly
+    #leaving the original in tact
+    if path.isfile(file_path):
+        import random
+        from os import listdir
+        file_base_name = path.splitext(path.basename(file_path))[0]
+        file_ext = path.splitext(path.basename(file_path))[1]
+        file_base_path = path.dirname(file_path)
+        existing_posts = [f for f in listdir(blog_root) 
+                            if '.rst' in f and path.isfile(f) and file_base_name in f]
+        file_base_name += '_' + str(len(existing_posts)) + file_ext
+        file_path = path.join(file_base_path, file_base_name)
+        
+
+    try:
+        post_file = open(file_path, 'w')
+        post_text = POST_TEMPLATE % pars
+        post_text += ("\n%s" % body)
+        post_file.write(post_text)
+
+        print('Blog post template created: %s\n' % path.basename(file_path))
+    except Exception, e:
+        #This may be an access right issue or something along the lines.
+        print(e)
+        raise e
+    finally:
+        if(post_file):
+            post_file.close()
+    
+
+    
+
+
+subparser = ablog_commands.add_parser('post',
+        help='creates a blank post with today''s date',
+        version=ablog.__version__)
+
+subparser.add_argument('-t', '--title', dest='post_title', type=str,
+    default='New Post',
+    help='path to your project built directory; default is `New Post`')
+subparser.add_argument('-b', '--body', dest='post_body', type=str,
+    default='',
+    help='path to your project built directory; default is blank.')
+subparser.set_defaults(func=lambda ns: ablog_post(**ns.__dict__))
+subparser.set_defaults(subparser=subparser)
 
 
 
