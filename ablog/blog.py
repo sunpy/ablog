45098cb0fae7304f17b3d1d52db6b7bb55eaf7de
Nabil <nabil.freij@gmail.com>
2017-07-31 17:11:20 +0200

    revert fix?

--- ablog/blog.py
+++ ablog/blog.py
@@ -394,7 +394,7 @@ class Post(BlogPageMixin):
 
         revise_pending_xrefs(doctree, pagename)
         # TODO: This should not be commented out, but it breaks it.
-        #app.env.resolve_references(doctree, pagename, app.builder)
+        app.env.resolve_references(doctree, pagename, app.builder)
 
         add_permalinks, app.builder.add_permalinks = (
             app.builder.add_permalinks, False)
