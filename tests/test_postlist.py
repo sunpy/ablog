68f0a21b84f555975cae29ceb6b71e5fc7ce270d
ykrods <890082+ykrods@users.noreply.github.com>
2020-12-22 16:24:53 +0900

    Add (rollback) sphinx2 test

--- tests/test_postlist.py
+++ tests/test_postlist.py
@@ -1,6 +1,15 @@
 import pytest
 
 
+def read_text(path):
+    """
+    Support function to give backward compatibility with older sphinx (v2).
+    """
+    if hasattr(path, "read_text"):
+        return path.read_text()
+    return path.text()
+
+
 @pytest.mark.sphinx("html", testroot="postlist")  # using roots/test-postlist
 def test_postlist(app, status, warning):
     app.build()
@@ -8,7 +17,7 @@ def test_postlist(app, status, warning):
     assert app.statuscode == 0
     assert (app.outdir / "postlist.html").exists()
 
-    html = (app.outdir / "postlist.html").read_text()
+    html = read_text(app.outdir / "postlist.html")
     assert '<ul class="postlist-style-none postlist simple">' in html
     assert (
         "<li><p>01 December - " '<a class="reference internal" href="post.html">post</a>' "</p></li>"
@@ -21,7 +30,7 @@ def test_postlist_date_format_conf(app, status, warning):
 
     assert app.statuscode == 0
 
-    html = (app.outdir / "postlist.html").read_text()
+    html = read_text(app.outdir / "postlist.html")
     assert (
         "<li><p>2020-12-01 - " '<a class="reference internal" href="post.html">post</a>' "</p></li>"
     ) in html
