4985f89951f174e47f51ef790306d89c08ee9e0e
Nabil Freij <nabil.freij@gmail.com>
2023-03-20 23:13:12 -0700

    Update version handling

--- src/ablog/version.py
+++ src/ablog/version.py
@@ -1,6 +1,17 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
 try:
-    version = get_distribution("ablog").version
-except DistributionNotFound:
-    version = "unknown.dev"
+    from ._version import version
+except Exception:
+    import warnings
+
+    warnings.warn(f'could not determine {__name__.split(".")[0]} package version; this indicates a broken installation')
+    del warnings
+
+    version = "0.0.0"
+
+from packaging.version import parse as _parse
+
+_version = _parse(version)
+major, minor, bugfix = [*_version.release, 0][:3]
+release = not _version.is_devrelease
+
+__all__ = ["version", "major", "minor", "bugfix", "release"]
